3411b5274fab72e20df77f5c7260f6b550bdb8d1
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2013-05-14 11:48:19 +0200

    [rpc] create angular json-rpc service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,42 @@ angular.module('flmUiApp', ['ngCookies', 'ui.bootstrap'])
         templateUrl: 'views/wifi.html',
         controller: 'WifiCtrl'
       })
+      .when('/status', {
+        templateUrl: 'views/status.html',
+        controller: 'StatusCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+  });
+
+angular.module('flmUiApp')
+  .factory('flmRpc', function ($rootScope, $http, $location, $q) {
+    return {
+      call: function (endpoint, method, params) {
+        var deferred = $q.defer();
+
+        var url = "/cgi-bin/luci/rpc/" + endpoint  + "?auth=" + $rootScope.sysauth;
+        var request = {
+          "method": method,
+          "params": params,
+          "id": Date.now()
+        };
+
+        $http.post(url, request)
+          .success(function(response) {
+            if (response.error) {
+              deferred.reject(response.error);
+            } else {
+              deferred.resolve(response.result);
+            }
+          })
+          .error(function(response) {
+            /* an invalid auth seems to trigger a 500 */
+            $location.path("/");
+          });
+
+        return deferred.promise;
+      }
+    };
   });
