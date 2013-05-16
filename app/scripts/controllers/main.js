bbf93c36d6537143300a16b8d1a8d422cf019927
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2013-05-16 15:55:26 +0200

    [main] fix indentation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,34 +1,33 @@
-'use strict';
+"use strict";
 
-angular.module('flmUiApp')
-  .controller('MainCtrl', function ($rootScope, $scope, $http, $location) {
-    $scope.user = "root";
-    $scope.pass = "root";
-    $scope.alerts = [];
+angular.module("flmUiApp")
+    .controller("MainCtrl", function ($rootScope, $scope, $http, $location) {
+        $scope.user = "root";
+        $scope.pass = "root";
+        $scope.alerts = [];
 
-    $scope.closeAlert = function(index) {
-        $scope.alerts.splice(index, 1);
-    };
-
-    $scope.rpcLogin = function() {
-        var url = "/cgi-bin/luci/rpc/auth";
-        var request = {
-            "method": "login",
-            "params": [$scope.user, $scope.pass],
-            "id": Date.now()
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
         };
 
-        $http.post(url, request).success(function(response) {
-            if (response.result) {
-                $rootScope.sysauth = response.result;
-                $location.path("/status");
-            } else {
-                $scope.alerts.push({
-                    type: "error",
-                    msg: "Wrong username or password. Please try again."
-                });
+        $scope.rpcLogin = function() {
+            var url = "/cgi-bin/luci/rpc/auth";
+            var request = {
+                "method": "login",
+                "params": [$scope.user, $scope.pass],
+                "id": Date.now()
             };
-        });
-    };
 
-  });
+            $http.post(url, request).success(function(response) {
+                if (response.result) {
+                    $rootScope.sysauth = response.result;
+                    $location.path("/status");
+                } else {
+                    $scope.alerts.push({
+                        type: "error",
+                        msg: "Wrong username or password. Please try again."
+                    });
+                };
+            });
+        };
+    });
